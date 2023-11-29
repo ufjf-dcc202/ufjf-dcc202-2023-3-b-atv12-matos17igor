ea27acd351cd889b0c7e2743c2304e0d55cf8eca
Igor Matos <igom17_pvp@hotmail.com>
2023-11-28 22:55:26 -0300

    adiciona funcao getEstoque

--- estoque.js
+++ estoque.js
@@ -1,1 +1,14 @@
-let estoque = {};
+let estoque = {
+    'joao': {
+        'maca': 1,
+        'banana': 2
+    },
+    'maria': {
+        'maca': 2,
+        'pera': 3
+    }
+};
+
+function getEstoque() {
+    return estoque;
+}
