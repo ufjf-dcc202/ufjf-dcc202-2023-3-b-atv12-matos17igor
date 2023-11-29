588bf1df462832e2545f5f5c7d502b98fc368bab
Igor Matos <igom17_pvp@hotmail.com>
2023-11-28 22:56:29 -0300

    cria main.js com as constantes

---
+++ main.js
@@ -0,0 +1,17 @@
+const olJoao = document.querySelector("#joao");
+const olMaria = document.querySelector("#maria");
+
+document.entrada.addEventListener('submit', leFormulario);
+
+function leFormulario(event) {
+    event.preventDefault();
+    const quantidade = Number(document.entrada.quantidade.value);
+    const fruta = document.entrada.fruta.value;
+    const origem = document.entrada.origem.value;
+    const destino = document.entrada.destino.value;
+
+    console.log(`${origem} doa ${quantidade} ${fruta} para ${destino}`);
+    // document.entrada.submit();
+}
+
+
